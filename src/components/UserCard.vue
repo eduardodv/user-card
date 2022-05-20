d355f82063f04f1a55185f3615bb48e07f1e7add
Eduardo Dalla Vecchia <eduardodvecchia@gmail.com>
2022-05-20 20:51:27 -0300

    Default card layout

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,15 +1,59 @@
 <template>
-  <main class="min-h-screen p-8">
-    <div class="w-96 p-5 max-w-full bg-blue-700 rounded-lg overflow-hidden shadow-lg">
-      <div class="w-20 h-20 rounded-full m-auto overflow-hidden">
-        <img src="/images/default-user.jpg" alt="Image default user">
+  <div>
+    <div class="relative min-h-480 text-center p-5 w-full bg-gradient-to-r from-green-300 to-blue-500 rounded-lg overflow-hidden shadow-lg">
+      <button class="dots absolute top-4 right-5">
+        <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300" />
+        <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300 delay-100" />
+        <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300 delay-200" />
+      </button>
+      <div class=" w-32 h-32 rounded-full m-auto overflow-hidden">
+        <img
+          src="/images/default-user.jpg"
+          alt="Image default user"
+          class="object-cover object-top w-full h-full"
+        >
+      </div>
+      <div class="capitalize mt-3">
+        <div class="bg-white inline-block py-0.5 px-3 rounded-lg min-h-max">
+          <b>{{ form.name ? form.name : 'Jhon Doe' }}{{ form.age ? `, ${form.age}` : '' }}</b>
+        </div>
+      </div>
+      <div class="mt-6">
+        <a
+          :href="`tel:${form.phone}`"
+          class="text-white inline-block hover:underline"
+        >
+          {{ form.phone ? form.phone : '(42) 9 9999-9999' }}
+        </a>
+      </div>
+      <div class="">
+        <a
+          :href="`mailto:${form.email}`"
+          class="text-white inline-block hover:underline"
+        >
+          {{ form.email ? form.email : 'email@email.com' }}
+        </a>
       </div>
     </div>
-  </main>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'UserComponent',
+  props: {
+    form: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
 }
 </script>
+
+<style>
+  .dots:hover span {
+    transform: scale(1.3);
+  }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,93 @@
 <template>
-  <div>
-    <UserCard />
-  </div>
+  <main class="bg-blue-100 w-full min-h-screen p-6 flex items-center justify-center">
+    <div class="w-96">
+      <UserCard :form="form" />
+      <form action="" class="bg-white mt-7 rounded-lg bg-white py-7 px-5 md:px-7">
+        <div class="">
+          <label
+            for="name"
+            class="cursor-pointer"
+          >Name:</label>
+          <input
+            id="name"
+            v-model="form.name"
+            type="text"
+            name="name"
+            class="w-full block border-2 rounded-xl py-1 px-3"
+          >
+        </div>
+        <div class="mt-3">
+          <label
+            for="phone"
+            class="cursor-pointer"
+          >Phone:</label>
+          <input
+            id="phone"
+            v-model="form.phone"
+            type="tel"
+            name="phone"
+            class="w-full block border-2 rounded-xl py-1 px-3"
+          >
+        </div>
+        <div class="mt-3">
+          <label
+            for="email"
+            class="cursor-pointer"
+          >E-mail:</label>
+          <input
+            id="email"
+            v-model="form.email"
+            type="email"
+            name="email"
+            class="w-full block border-2 rounded-xl py-1 px-3"
+          >
+        </div>
+        <div class="mt-3">
+          <label
+            for="age"
+            class="cursor-pointer"
+          >Age:</label>
+          <input
+            id="age"
+            v-model="form.age"
+            type="number"
+            name="age"
+            min="0"
+            max="150"
+            value="0"
+            class="w-full block border-2 rounded-xl py-1 px-3"
+          >
+        </div>
+        <div class="mt-3">
+          <label
+            for="file"
+            class="cursor-pointer"
+          >Picture:</label>
+          <input
+            id="file"
+            type="file"
+            name="file"
+            class="w-full block border-2 rounded-xl py-1 px-3"
+          >
+        </div>
+      </form>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      form: {
+        name: null,
+        phone: null,
+        email: null,
+        age: null,
+        file: null
+      }
+    }
+  }
 }
 </script>
