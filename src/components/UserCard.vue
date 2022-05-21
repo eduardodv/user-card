1552acb387c0400beb7292335f527d9aaff5177a
Eduardo Dalla Vecchia <eduardodvecchia@gmail.com>
2022-05-21 20:31:30 -0300

    Card layout done

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,38 +1,49 @@
 <template>
-  <div>
-    <div class="relative min-h-480 text-center p-5 w-full bg-gradient-to-r from-green-300 to-blue-500 rounded-lg overflow-hidden shadow-lg">
-      <button class="dots absolute top-4 right-5">
-        <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300" />
-        <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300 delay-100" />
-        <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300 delay-200" />
-      </button>
-      <div class=" w-32 h-32 rounded-full m-auto overflow-hidden">
-        <img
-          src="/images/default-user.jpg"
-          alt="Image default user"
-          class="object-cover object-top w-full h-full"
-        >
-      </div>
-      <div class="capitalize mt-3">
-        <div class="bg-white inline-block py-0.5 px-3 rounded-lg min-h-max">
-          <b>{{ form.name ? form.name : 'Jhon Doe' }}{{ form.age ? `, ${form.age}` : '' }}</b>
+  <div id="card" class="lg:flex lg:order-2 lg:w-1/2 transition ease-in-out duration-400">
+    <div class="lg:flex lg:items-center lg:justify-center lg:min-h-full relative text-center p-5 w-full bg-gradient-to-r from-green-300 to-blue-500 rounded-lg overflow-hidden shadow-lg">
+      <div>
+        <button class="dots absolute top-3 right-2 py-1 px-3">
+          <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300" />
+          <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300 delay-100" />
+          <span class="block w-1.5 h-1.5 rounded-full my-1 bg-white transition ease-in-out duration-300 delay-200" />
+        </button>
+        <div class=" w-32 h-32 rounded-full m-auto overflow-hidden bg-blue-500">
+          <img
+            v-if="form.file"
+            :src="form.file.url"
+            alt="User image"
+            class="object-cover object-top w-full h-full"
+          >
+          <img
+            v-else
+            src="/images/default-user.jpg"
+            alt="Image default user"
+            class="object-cover object-top w-full h-full"
+          >
         </div>
-      </div>
-      <div class="mt-6">
-        <a
-          :href="`tel:${form.phone}`"
-          class="text-white inline-block hover:underline"
-        >
-          {{ form.phone ? form.phone : '(42) 9 9999-9999' }}
-        </a>
-      </div>
-      <div class="">
-        <a
-          :href="`mailto:${form.email}`"
-          class="text-white inline-block hover:underline"
-        >
-          {{ form.email ? form.email : 'email@email.com' }}
-        </a>
+        <div class="mt-3">
+          <div class="bg-white inline-block py-0.5 px-3 rounded-lg min-h-max">
+            <!-- <b v-html="form.name ? teste : 'Jhon Doe'" /> -->
+            <b>{{ form.name ? form.name : 'Jhon Doe' }}</b>
+            <b>{{ form.age ? `, ${form.age}` : ', 28' }}</b>
+          </div>
+        </div>
+        <div class="mt-6">
+          <a
+            :href="`tel:${form.phone}`"
+            class="text-white text-lg inline-block hover:underline"
+          >
+            {{ form.phone ? form.phone : '(42) 9 9999-9999' }}
+          </a>
+        </div>
+        <div class="">
+          <a
+            :href="`mailto:${form.email}`"
+            class="text-white text-lg inline-block hover:underline"
+          >
+            {{ form.email ? form.email : 'email@email.com' }}
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -48,6 +59,22 @@ export default {
         return {}
       }
     }
+  },
+  computed: {
+    teste () {
+      const name = this.form.name.split('')
+      let letters = ''
+      name.forEach((element) => {
+        letters += `<span>${element}</span>`
+      })
+      return letters
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+
   }
 }
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,20 @@
 <template>
   <main class="bg-blue-100 w-full min-h-screen p-6 flex items-center justify-center">
-    <div class="w-96">
+    <div class="w-96 lg:w-full max-w-3xl lg:flex lg:gap-5">
       <UserCard :form="form" />
-      <form action="" class="bg-white mt-7 rounded-lg bg-white py-7 px-5 md:px-7">
+      <form
+        class="lg:order-1 lg:w-1/2 bg-white mt-7 lg:mt-0 rounded-lg bg-white py-7 px-5 md:px-7"
+        @submit.prevent="submit"
+      >
         <div class="">
           <label
             for="name"
-            class="cursor-pointer"
+            class="cursor-pointer text-sm mb-1 inline-block"
           >Name:</label>
           <input
             id="name"
             v-model="form.name"
+            required
             type="text"
             name="name"
             class="w-full block border-2 rounded-xl py-1 px-3"
@@ -19,11 +23,12 @@
         <div class="mt-3">
           <label
             for="phone"
-            class="cursor-pointer"
+            class="cursor-pointer text-sm mb-1 inline-block"
           >Phone:</label>
           <input
             id="phone"
             v-model="form.phone"
+            required
             type="tel"
             name="phone"
             class="w-full block border-2 rounded-xl py-1 px-3"
@@ -32,11 +37,12 @@
         <div class="mt-3">
           <label
             for="email"
-            class="cursor-pointer"
+            class="cursor-pointer text-sm mb-1 inline-block"
           >E-mail:</label>
           <input
             id="email"
             v-model="form.email"
+            required
             type="email"
             name="email"
             class="w-full block border-2 rounded-xl py-1 px-3"
@@ -45,11 +51,12 @@
         <div class="mt-3">
           <label
             for="age"
-            class="cursor-pointer"
+            class="cursor-pointer text-sm mb-1 inline-block"
           >Age:</label>
           <input
             id="age"
             v-model="form.age"
+            required
             type="number"
             name="age"
             min="0"
@@ -58,17 +65,31 @@
             class="w-full block border-2 rounded-xl py-1 px-3"
           >
         </div>
-        <div class="mt-3">
+        <div class="mt-5 relative">
           <label
             for="file"
-            class="cursor-pointer"
-          >Picture:</label>
+            role="button"
+            class="cursor-pointer text-sm mb-1 block border-dashed border-2 hover:border-blue-500 transition ease-in-out duration-200 py-5 px-4 text-center"
+          >
+            {{ form.file ? form.file.name : 'UPLOAD IMAGE' }}
+          </label>
           <input
             id="file"
+            required
             type="file"
             name="file"
-            class="w-full block border-2 rounded-xl py-1 px-3"
+            class="opacity-0 absolute top-0 left-0"
+            accept="image/*"
+            @change="filePicture"
           >
+        </div>
+        <div class="mt-5 text-center">
+          <button
+            type="submit"
+            class="bg-blue-500 text-white font-bold uppercase px-10 py-2 rounded-full hover:bg-green-500 transition ease-in-out duration-300"
+          >
+            Send
+          </button>
         </div>
       </form>
     </div>
@@ -76,10 +97,12 @@
 </template>
 
 <script>
+
 export default {
   name: 'IndexPage',
   data () {
     return {
+      customFile: null,
       form: {
         name: null,
         phone: null,
@@ -88,6 +111,29 @@ export default {
         file: null
       }
     }
+  },
+  methods: {
+    filePicture (event) {
+      const file = event.target.files[0]
+      if (file) {
+        this.form.file = {
+          name: file.name,
+          url: URL.createObjectURL(file)
+        }
+      }
+    },
+
+    submit () {
+      // this.form = {
+      //   name: null,
+      //   phone: null,
+      //   email: null,
+      //   age: null,
+      //   file: null
+      // }
+      console.log(this.form)
+    }
+
   }
 }
 </script>
